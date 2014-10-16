1ce4d9d7533c86838f54fa36b402e52f888de7f3
Mark Odell <dracone.cordis@gmail.com>
2014-10-15 21:45:52 -0700

    Way to go mark. You ruined the moment.

---
+++ set_types/wci/frontend/pascal/parsers/SetTypeParser.java
@@ -0,0 +1,40 @@
+package wci.frontend.pascal.parsers;
+
+import wci.frontend.Token;
+import wci.frontend.pascal.PascalParserTD;
+import wci.intermediate.ICodeNode;
+import wci.intermediate.TypeFactory;
+import wci.intermediate.TypeSpec;
+import wci.intermediate.typeimpl.TypeFormImpl;
+import wci.intermediate.typeimpl.TypeKeyImpl;
+
+public class SetTypeParser extends TypeSpecificationParser {
+
+	public SetTypeParser(PascalParserTD parent) {
+		super(parent);
+	}
+	
+	public TypeSpec parse(Token token)
+		throws Exception
+	{
+		// See word set GOOODBYE BABY!
+		nextToken(); // Nom
+		token = nextToken(); // Nom
+		
+		// IF an previously defined Type
+		// (Look at Symbol Table)
+		// 		Create a new Set TypeSpec to return
+		//		Assign TypeSpec attr SET_ELEMENT_TYPE to typespec saved in prev Identifier
+		//		return TypeSpec
+		// ELSE 
+		
+		SimpleTypeParser newParser = new SimpleTypeParser(this);
+		TypeSpec aType = newParser.parse(token);
+		
+		TypeSpec aSetType = TypeFactory.createType(TypeFormImpl.SET);
+		aSetType.setAttribute(TypeKeyImpl.SET_ELEMENT_TYPE, aType);
+		
+		return aType;
+	}
+
+}
